af844d41cb2c4239069cb6e35ba3538890aadbc7
Jaeun Choi <wodns617@gmail.com>
2023-03-22 01:06:25 +0900

    ch02 revision

--- sp5-chap02/src/main/java/chap02/Main.java
+++ sp5-chap02/src/main/java/chap02/Main.java
@@ -7,8 +7,11 @@ public class Main {
         AnnotationConfigApplicationContext ctx =
                 new AnnotationConfigApplicationContext(AppContext.class);
         Greeter g = ctx.getBean("greeter",Greeter.class);
+        Greeter g1 = ctx.getBean("greeter", Greeter.class);
         String msg = g.greet("스프링");
         System.out.println(msg);
+        //빈 객체는 싱글톤 범위를 갖는다.
+        System.out.println(g==g1);
         ctx.close();
     }
 }
